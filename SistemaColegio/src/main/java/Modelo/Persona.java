3515ce4143dca32d8190750532507f701f81922e
USUARIO <Yeidy Caicedo>
2026-04-14 02:45:09 -0500

    Se creo la clase padre "Persona", con atributos generales. Además el método para mostrar esta información, y los getters.

--- SistemaColegio/src/main/java/Modelo/Persona.java
+++ SistemaColegio/src/main/java/Modelo/Persona.java
@@ -10,4 +10,38 @@ package Modelo;
  */
 public class Persona {
     
+protected String nombre;
+    protected String direccion;
+    protected String telefono;
+    protected String fechaNacimiento;
+
+    public Persona(String nombre, String direccion, String telefono, String fechaNacimiento) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    
+    public String generarReporte() {
+        return "Nombre: " + nombre 
+                + "\n" + "Telefono: " + telefono 
+                + "\n" + "Direccion: " + direccion 
+                + "\n" + "Fecha de nacimiento: " + fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
 }
