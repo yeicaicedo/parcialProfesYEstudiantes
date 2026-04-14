32bf1f897a7a92cf5400533756887342c37494fd
USUARIO <Yeidy Caicedo>
2026-04-14 03:31:08 -0500

    Se realizó el test del modelo Persona.

--- SistemaColegio/src/test/java/Modelo/PersonaTest.java
+++ SistemaColegio/src/test/java/Modelo/PersonaTest.java
@@ -16,94 +16,21 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author USUARIO
  */
 public class PersonaTest {
-    
+
     public PersonaTest() {
     }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
 
-    /**
-     * Test of generarReporte method, of class Persona.
-     */
     @Test
-    public void testGenerarReporte() {
-        System.out.println("generarReporte");
-        Persona instance = null;
-        String expResult = "";
-        String result = instance.generarReporte();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+    public void testGenerarReportePersona() {
 
-    /**
-     * Test of getNombre method, of class Persona.
-     */
-    @Test
-    public void testGetNombre() {
-        System.out.println("getNombre");
-        Persona instance = null;
-        String expResult = "";
-        String result = instance.getNombre();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        Persona p = new Persona("Laura", "Calle 10", "3001234567", "01/01/2000");
 
-    /**
-     * Test of getDireccion method, of class Persona.
-     */
-    @Test
-    public void testGetDireccion() {
-        System.out.println("getDireccion");
-        Persona instance = null;
-        String expResult = "";
-        String result = instance.getDireccion();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        String esperado
+                = "Nombre: Laura\n"
+                + "Telefono: 3001234567\n"
+                + "Direccion: Calle 10\n"
+                + "Fecha de nacimiento: 01/01/2000";
 
-    /**
-     * Test of getTelefono method, of class Persona.
-     */
-    @Test
-    public void testGetTelefono() {
-        System.out.println("getTelefono");
-        Persona instance = null;
-        String expResult = "";
-        String result = instance.getTelefono();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(esperado, p.generarReporte(), "Fallo en el test1");
     }
-
-    /**
-     * Test of getFechaNacimiento method, of class Persona.
-     */
-    @Test
-    public void testGetFechaNacimiento() {
-        System.out.println("getFechaNacimiento");
-        Persona instance = null;
-        String expResult = "";
-        String result = instance.getFechaNacimiento();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
