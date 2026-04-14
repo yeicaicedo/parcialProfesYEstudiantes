69c3b374f68519ae5271052562637834adc7e585
USUARIO <Yeidy Caicedo>
2026-04-14 02:56:27 -0500

    Se creó la clase hija "Profesor", con atributos adicionales. Se crearon los métodos para calcular el pago mensual teniendo en cuenta el recargo del 30% y calcular también las prestaciones sociales al 19% del pago mensual. Se implementó la sobrecarga del método generarReporte, permitiendo mostrar la información general de la persona junto con los otros atributos de cada profesor.

--- SistemaColegio/src/main/java/Modelo/Profesor.java
+++ SistemaColegio/src/main/java/Modelo/Profesor.java
@@ -8,6 +8,72 @@ package Modelo;
  *
  * @author USUARIO
  */
-public class Profesor {
+public class Profesor extends Persona {
+
+    private int cedula;
+    private String area;
+    private double salarioHora;
+    private int horasTrabajadas;
+
+    public Profesor(String nombre, String direccion, String telefono, String fechaNacimiento,
+            int cedula, String area, double salarioHora, int horasTrabajadas) {
+
+        super(nombre, direccion, telefono, fechaNacimiento);
+        this.cedula = cedula;
+        this.area = area;
+        this.salarioHora = salarioHora;
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
     
+    public double calcularPagoMensual() {
+        double pagoMensual = salarioHora * horasTrabajadas;
+        return pagoMensual + (pagoMensual * 0.30);
+    }
+
+    public double calcularPrestaciones() {
+        return calcularPagoMensual() * 0.19;
+    }
+
+    @Override
+    public String generarReporte() {
+        return super.generarReporte()
+                + "\n" + "Cedula: " + cedula
+                + "\n" + "Area: " + area
+                + "\n" + "Pago mensual: $" + calcularPagoMensual()
+                + "\n" + "Prestaciones: $" + calcularPrestaciones();
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public double getSalarioHora() {
+        return salarioHora;
+    }
+
+    public void setSalarioHora(double salarioHora) {
+        this.salarioHora = salarioHora;
+    }
+
+    public int getHorasTrabajadas() {
+        return horasTrabajadas;
+    }
+
+    public void setHorasTrabajadas(int horasTrabajadas) {
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
 }
