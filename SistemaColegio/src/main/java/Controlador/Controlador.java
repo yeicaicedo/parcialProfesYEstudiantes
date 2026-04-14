8cf6e4b2430dd9e32d7e80f424b1533a9cc22bc2
USUARIO <Yeidy Caicedo>
2026-04-14 03:13:34 -0500

    Se creó el primer método en el controlador para validar que la fecha esté en el formato dd/MM/yyyy. Se utilizó try/catch para que si muestra error por tener la fecha mal no se salga del sistema sino que lo pida nuevamente.

--- SistemaColegio/src/main/java/Controlador/Controlador.java
+++ SistemaColegio/src/main/java/Controlador/Controlador.java
@@ -8,6 +8,37 @@ package Controlador;
  *
  * @author USUARIO
  */
+import Modelo.Colegio;
+import Modelo.Profesor;
+import Modelo.Estudiante;
+
 public class Controlador {
-    
+    private Colegio colegio;
+
+    public Controlador() {
+        colegio = new Colegio();
+    }
+
+    public boolean validarFecha(String fechaNacimiento) {
+        String partes[] = fechaNacimiento.split("/");
+        if (partes.length != 3) {
+            return false;
+        }
+        try {
+            int dia = Integer.parseInt(partes[0]);
+            int mes = Integer.parseInt(partes[1]);
+            int anio = Integer.parseInt(partes[2]);
+
+            if (dia < 1 || dia > 31) {
+                return false;
+            }
+            if (mes < 1 || mes > 12) {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
 }
